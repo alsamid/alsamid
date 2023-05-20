1855075f1b2b31cb00977e6f75939bbff71fbe43
SAMED <samedalislam@gmail.com>
2023-05-20 20:55:21 +0600

    Folder Structure

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>AL SAMID</title>
+      </Head>
+      <div>Home</div>
+    </>
+  );
+};
+export default Home;
